26c82090e202fe88f85367f0c52b3af803c00162
Szymon Jereczek <szymon230598@gmail.com>
2023-01-18 20:29:46 +0100

    Revert "Initial commit"
    This reverts commit d825b2eed8bc3a76496ff77a0befc30e93fcca85.

--- src/main/java/jereczek/dziennikocen/tables/student/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package jereczek.dziennikocen.tables.student;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student, Integer> {
-    Student findStudentByNrIndeksu(int nrIndeksu);
-}
--- src/main/java/jereczek/dziennikocen/tables/wydzial/WydzialRepo.java
+++
@@ -1,7 +0,0 @@
-package jereczek.dziennikocen.tables.wydzial;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WydzialRepo extends JpaRepository<Wydzial, String> {
-    Wydzial findByNazwaWydzialu(String nazwa_wydzialu);
-}
